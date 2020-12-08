a4f096f59ab587761140e7ba2ef33434fbe87c1b
xIncrementx <73523231+xIncrementx@users.noreply.github.com>
2020-12-08 15:14:21 +0100

    Added comments

--- KortKlubbenServer/GameServer/src/game/Card.java
+++ KortKlubbenServer/GameServer/src/game/Card.java
@@ -51,6 +51,8 @@ public class Card {
             default:
                 this.fullName += value;
                 break;
+
+
         }
     }
 }
--- KortKlubbenServer/GameServer/src/game/cardFactory.java
+++ KortKlubbenServer/GameServer/src/game/cardFactory.java
@@ -3,19 +3,34 @@ package game;
 import java.util.ArrayList;
 
 public class CardFactory {
-
+    /**
+     * Bla bla bla bla
+     * @param colour Black or red
+     * @return Returns a new card
+     */
     private Card CreateCard(byte value, String suit, String colour){
         return new Card(value, suit, colour);
     }
 
-    public ArrayList[Card] CreateDeck(){
-        ArrayList<Card> deck = new ArrayList<>();
+    String gayAss () {
+        return "";
+    }
 
+    public ArrayList<Card>CreateDeck(){
+        ArrayList<Card> deck = new ArrayList<>();
+        
         for (byte b = 1; b < 14; b++){
             deck.add(new Card(b, "Hjerter", "Rød"));
             deck.add(new Card(b, "Ruder", "Rød"));
             deck.add(new Card(b, "Klør", "Sort"));
             deck.add(new Card(b, "Spar", "Sort"));
         }
+        
+        deck.add(new Card((byte) 14, "", "Rød"));
+        deck.add(new Card((byte) 14, "", "Rød"));
+        deck.add(new Card((byte) 14, "", "Sort"));
+        deck.add(new Card((byte) 14, "", "Sort"));
+
+        return deck;
     }
 }
